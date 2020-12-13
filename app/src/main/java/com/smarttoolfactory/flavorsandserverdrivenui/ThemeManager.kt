e27b63557b7e90a357fb7bd7644886f62a63a345
Smart Tool Factory <smarttoolfactory@icloud.com>
2020-12-13 22:14:27 +0300

    Add splash screen for 2 flavors

---
+++ app/src/main/java/com/smarttoolfactory/flavorsandserverdrivenui/ThemeManager.kt
@@ -0,0 +1,8 @@
+package com.smarttoolfactory.flavorsandserverdrivenui
+
+/**
+ * Color, theme, style management class based on config file retrieved from remote data source
+ */
+class ThemeManager {
+
+}
